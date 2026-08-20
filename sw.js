3fc379ef514054b5a7a79a55d8c22cbca11b5a1e
mn4w4656pt-ux <mn4w4656pt@privaterelay.appleid.com>
2026-08-20 22:32:11 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='asset-v7';const F=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request))));
